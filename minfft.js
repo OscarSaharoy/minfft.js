3afb813d75657bf6d5dd170f22e4b7940fe9c430
Oscar Saharoy <osaharoy@gmail.com>
2021-01-17 23:57:40 +0000

    minor improvements

--- minfft.js
+++ minfft.js
@@ -7,43 +7,9 @@ function fft( inputSignal, frequencySpectrum = true ) {
 
     const x = inputSignal; // input signal - must be power of 2 and >= 2
     const N = x.length;    // sequence length
-    const X = Array(N);    // fourier coefficients (frequency domain)
 
-    // handle base case where N==2
-    if( N == 2 ) {
-
-        X[0] = { re: x[0] + x[1], im: 0 };
-        X[1] = { re: x[0] - x[1], im: 0 };
-
-        return X;
-    }
-
-    // split samples into even and odd
-    const evens = [];
-    const odds  = [];
-
-    for(var i=0; i<N; ++i) {
-
-        if( i%2 ) odds.push( x[i] );
-        else     evens.push( x[i] );
-    }
-
-    // do fft on even and odd samples
-    const E = fft(evens, false);
-    const O = fft(odds,  false);
-
-    // butterfly the results in E and O
-    for(var k=0; k<N/2; ++k) {
-
-        const Wre     = Math.cos( 6.283185*k/N );
-        const Wim     = Math.sin( 6.283185*k/N );
-
-        const W_Ok_re = Wre * O[k].re - Wim * O[k].im;
-        const W_Ok_im = Wim * O[k].re + Wre * O[k].im;
-
-        X[k]          = { re: E[k].re + W_Ok_re, im: E[k].im + W_Ok_im };
-        X[k+N/2]      = { re: E[k].re - W_Ok_re, im: E[k].im - W_Ok_im };
-    }
+    // call _fft2 to get the complex fourier coefficents
+    const X = _fft2( x, 0, 1, N );
 
     // return the complex fourier coefficients if we don't want the frequency spectrum
     if( !frequencySpectrum ) return X;
@@ -58,7 +24,9 @@ function fft( inputSignal, frequencySpectrum = true ) {
 function _fft2( x, iStart, step, N ) {
 
     const X = Array(N);
+    const b = Array(N);
 
+    // handle base case where N==2
     if( N==2 ) {
 
         X[0] = { re: x[iStart] + x[iStart + step], im: 0 };
@@ -67,7 +35,7 @@ function _fft2( x, iStart, step, N ) {
         return X;
     }
 
-    // do fft on even and odd samples
+    // get fft of odd and even subsequences
     const E = _fft2( x, iStart,      step*2, N/2 );
     const O = _fft2( x, iStart+step, step*2, N/2 );
 
